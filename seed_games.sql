4bbab88b50dfb91dec90cabf55d3f0653d03e8c2
AZumD <anthon.zumendorf@gmail.com>
2025-05-31 17:07:40 +0200

    Seed games into database

---
+++ seed_games.sql
@@ -0,0 +1,147 @@
+INSERT INTO games (
+        title_sv, title_en, description_sv, description_en,
+        players, time, age, tags, img, rules
+    ) VALUES (
+        'Kortlek',
+        'Deck of cards',
+        'Varför göra det komplicerat? Självklart har vi även kortlekar.',
+        'Why make it complicated?',
+        '1-?',
+        '? min',
+        '6+',
+        '2p,strategy,family,party',
+        'https://azumd.github.io/bradspelsmeny/img/cards-pxl.webp',
+        'https://blogs.glowscotland.org.uk/re/public/west/uploads/sites/2327/2019/02/Card-Games.pdf'
+    );
+
+INSERT INTO games (
+        title_sv, title_en, description_sv, description_en,
+        players, time, age, tags, img, rules
+    ) VALUES (
+        'The Resistance',
+        'The Resistance',
+        'Ett socialt bluffspel där du aldrig kan lita på någon.',
+        'A social deduction game of trust and betrayal.',
+        '5–10',
+        '30 min',
+        '13+',
+        'social,party',
+        'https://azumd.github.io/bradspelsmeny/img/the-resistance-pxl.webp',
+        'The Resistance Rulebook.pdf'
+    );
+
+INSERT INTO games (
+        title_sv, title_en, description_sv, description_en,
+        players, time, age, tags, img, rules
+    ) VALUES (
+        'Equinox',
+        'Equinox',
+        'Ett fantasifullt kortspel där mystiska varelser tävlar om att gå till historien.',
+        'A whimsical game of mythical creatures competing for survival in legends.',
+        '2–5',
+        '45 min',
+        '10+',
+        'strategy,family,2p',
+        'https://azumd.github.io/bradspelsmeny/img/equinox-pxl.webp',
+        'https://cdn.1j1ju.com/medias/2b/62/72-equinox-rulebook.pdf'
+    );
+
+-- Additional games
+INSERT INTO games (
+    title_sv, title_en, description_sv, description_en, players, time, age, tags, img, rules
+) VALUES (
+    'Codenames: Duet',
+    'Codenames Duet',
+    'Ett kooperativt ordspel där ni tillsammans försöker hitta alla agenter.',
+    'A cooperative word game where you work together to find all the agents.',
+    '2',
+    '15–30 min',
+    '11+',
+    'word game,cooperative,puzzle',
+    'https://azumd.github.io/bradspelsmeny/img/codenames-duet-pxl.webp',
+    'https://czechgames.com/files/rules/codenames-duet-rules-en.pdf'
+);
+INSERT INTO games (
+    title_sv, title_en, description_sv, description_en, players, time, age, tags, img, rules
+) VALUES (
+    'Love Letter',
+    'Love Letter',
+    'Ett snabbt kortspel där du försöker leverera ditt kärleksbrev till prinsessan.',
+    'A quick card game where you try to deliver your love letter to the princess.',
+    '2–4',
+    '20 min',
+    '10+',
+    'strategy,card game,romance',
+    'https://azumd.github.io/bradspelsmeny/img/love-letter-pxl.webp',
+    'https://www.zmangames.com/en/products/love-letter/'
+);
+INSERT INTO games (
+    title_sv, title_en, description_sv, description_en, players, time, age, tags, img, rules
+) VALUES (
+    'Scout',
+    'Scout',
+    'Ett smart kortspel där du inte kan byta ordning på din hand och måste fatta taktiska beslut.',
+    'A clever card game where you can''t reorder your hand and must make tactical decisions.',
+    '2–5',
+    '20 min',
+    '9+',
+    'card game,strategy,tactical',
+    'https://azumd.github.io/bradspelsmeny/img/scout-pxl.webp',
+    'https://boardgamegeek.com/filepage/226090/scout-english-rules'
+);
+INSERT INTO games (
+    title_sv, title_en, description_sv, description_en, players, time, age, tags, img, rules
+) VALUES (
+    'Sriracha: Spelet',
+    'Sriracha: The Game',
+    'Ett snabbt kortspel inspirerat av den berömda heta såsen.',
+    'A fast-paced card game inspired by the famous hot sauce.',
+    '2–4',
+    '10–15 min',
+    '7+',
+    'party,quick,card game',
+    'https://azumd.github.io/bradspelsmeny/img/sriracha-pxl.webp',
+    'https://www.srirachathegame.com/rules'
+);
+INSERT INTO games (
+    title_sv, title_en, description_sv, description_en, players, time, age, tags, img, rules
+) VALUES (
+    'Zombie Fluxx',
+    'Zombie Fluxx',
+    'Ett kaotiskt kortspel där reglerna och målet ständigt förändras – nu med zombies!',
+    'A chaotic card game where the rules and goals constantly change – now with zombies!',
+    '2–6',
+    '10–40 min',
+    '8+',
+    'party,zombie,chaotic',
+    'https://azumd.github.io/bradspelsmeny/img/zombie-fluxx-pxl.webp',
+    'https://www.looneylabs.com/sites/default/files/literature/ZombieFluxx-Rules_0.pdf'
+);
+INSERT INTO games (
+    title_sv, title_en, description_sv, description_en, players, time, age, tags, img, rules
+) VALUES (
+    'Nepo Babies',
+    'Nepo Babies',
+    'Ett satiriskt partyspel om kändisbarn och privilegier.',
+    'A satirical party game about celebrity kids and privilege.',
+    '3–6',
+    '20–45 min',
+    '17+',
+    'party,humor,adult,satire',
+    'https://azumd.github.io/bradspelsmeny/img/nepo-babies-pxl.webp',
+    'https://nepobabiesgame.com/pages/how-to-play'
+);
+INSERT INTO games (
+    title_sv, title_en, description_sv, description_en, players, time, age, tags, img, rules
+) VALUES (
+    'Backgammon',
+    'Backgammon',
+    'Ett klassiskt strategispel för två spelare med rötter som sträcker sig tusentals år bakåt.',
+    'A classic strategy game for two players with roots going back thousands of years.',
+    '2',
+    '30–60 min',
+    '8+',
+    'classic,strategy,abstract',
+    'https://azumd.github.io/bradspelsmeny/img/backgammon-pxl.webp',
+    'https://bkgm.com/rules.html'
+);
